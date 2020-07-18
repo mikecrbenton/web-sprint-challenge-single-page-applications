2e28c679f16b13ce1a884fd5535442e0203e6799
mikecrbenton <mikecrbenton@hotmail.com>
2020-07-18 11:33:24 -0500

    started form - need state

--- src/form.js
+++ src/form.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styled from 'styled-components'
+
 
 function Form() {
+   
+   
+   
    return(
-      <div>form works</div>
+      <div>
+         <StyledForm>
+            {/* IMAGE HERE */}
+
+            <label htmlFor="name">
+               Name
+               <input
+                  type='text'
+                  name='name'
+                  value={}
+               />
+            </label>
+         </StyledForm>
+      </div>
    )
 }
 
 export default Form;
+
+const StyledForm = styled.form`
+
+`;
