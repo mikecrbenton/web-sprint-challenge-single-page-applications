4df7ed4f70bdfd6c4e62c44be5169ebd55063e97
mikecrbenton <mikecrbenton@hotmail.com>
2020-07-18 12:56:35 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import React from "react";
 import { Link, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import Form from './form';
 import Home from './home';
 
 const App = () => {
   return (
     <div>
-      <div>
-         <h1>Pizza World</h1>
-         <div>
-            <Link to="/">Home</Link>
-            <Link to="/form">Form</Link>
-         </div>
-      </div>
 
-    <Switch>
-       <Route path="/form">
-         <Form/>
-       </Route>
-       <Route path="/">
-         <Home/>
-       </Route>
-    </Switch>
+      <Navbar>
+         <nav>
+            <h1>Pizza World</h1>
+            <div>
+               <Link to="/">Home</Link>
+               <Link to="/form">Form</Link>
+            </div>
+         </nav>
+      </Navbar>
+
+      <Switch>
+         <Route path="/form">
+            <Form/>
+         </Route>
+         <Route path="/">
+            <Home/>
+         </Route>
+      </Switch>
 
    </div>
   );
 };
 export default App;
+
+const Navbar = styled.div`
+   border: solid 6px red;
+
+   nav{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0 2em;
+
+      h1{
+         font-size: 2rem;
+         margin: .5em 0;
+      }
+      a{
+         padding: 0 .5em;
+         text-decoration: none;
+         color: black;
+         font-weight: 700;
+         font-size: 1.2rem;
+      }
+   }
+
+`
--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import pizzaImage from './ivan-torres-MQUqbmszGGM-unsplash.jpg'
+
 
 function Home() {
    return(
